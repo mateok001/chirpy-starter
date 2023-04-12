4faad4c002941774aef30994bd1fe34ba2b3eb33
mateok001 <mateo163@163.com>
2023-04-12 15:50:55 +0800

    local deploy favicon

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230412.093948';
+const cacheName = 'chirpy-20230412.154640';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
